3c8c8e85b61cd12248e6e8f29675fc915600d010
Frank-Gu-81 <gushaopengfrank@gmail.com>
2025-01-21 22:22:58 -0600

    temp fix for rating history

--- src/components/RatingHistory/RatingHistory.jsx
+++ src/components/RatingHistory/RatingHistory.jsx
@@ -1,15 +1,54 @@
 import Post from '../Post/Post';
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../utilities/firebase";
 import { Container, Box, Stack, Typography } from '@mui/material';
 import AppBar from '../AppBar/AppBar';
 import NavigationBar from '../NavigationBar/NavigationBar';
 import './RatingHistory.css';
 import Avatar from '@mui/material/Avatar';
+import { useState, useEffect } from 'react';
 
 
-function RatingHistory({ userName, profilePic, filteredPost }) {
+function RatingHistory({ userName, profilePic }) {
+    const [filteredPost, setFilteredPost] = useState([]);
 
     const fake_friends = ["Alice", "Bob", "Charlie", "David"];
  // Add userName as a dependency to prevent it from running infinitely    
+
+    async function getPostsFromDB() {
+        try {
+            const querySnapshot = await getDocs(collection(db, "posts"));
+            const posts = [];
+            querySnapshot.forEach((doc) => {
+                if (doc.data().username === userName?.displayName){
+                    posts.push({ id: doc.id, ...doc.data() });
+                }
+            });
+            return posts;
+        } catch (error) {
+            console.log("Error: ", error);
+        }
+
+    }
+
+    useEffect(() => {
+        async function fetchPosts() {
+            try {
+                const fetchedPosts = await getPostsFromDB();
+                setFilteredPost(fetchedPosts);
+                // console.log("Filtered posts: ", fetchedPosts);
+            } catch (error) {
+                console.error("Error fetching posts: ", error);
+            }
+        }
+        fetchPosts();
+    }, [userName]); // Add userName as a dependency to prevent it from running infinitely
+
+    // print out the posts
+    useEffect(() => {
+        console.log(filteredPost);
+    }
+    , [filteredPost]);
 
     return (
         <div>
@@ -38,7 +77,7 @@ function RatingHistory({ userName, profilePic, filteredPost }) {
                 <NavigationBar />
             </Container>
         </div>
-    )
+    );
 
 }
 
